c6b734ddd4ab2da7702b3ce77c08c3bc1cbf0e93
JasonJiYork <jasonji@my.yorku.ca>
2022-03-29 13:27:06 -0400

    update lab 3

--- sample_site/app.py
+++ sample_site/app.py
@@ -18,12 +18,25 @@ def index():
     f.write(str(count))
     f.close()
 
-    # Render HTML with count variable
-    return render_template("index.html", count=count)
+    # Load current winners
+    f = open("winners.txt", "r")
+    winners = int(f.read())
+    f.close()
+
+    # Increment the winners
+    if count % 3 == 0:
+        winners += 1
+
+    # Overwrite the winners
+    f = open("winners.txt", "w")
+    f.write(str(winners))
+    f.close()
+
+    # Render HTML with count and winner variables
+    return render_template("index.html", count=count, winners=winners)
+
 
 
 if __name__ == "__main__":
     app.run()
 
-if 5 < 2:
-    print("123")

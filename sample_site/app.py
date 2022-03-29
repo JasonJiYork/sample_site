53aa2d41b6e533005041fc5a051112d30610da60
JasonJiYork <jasonji@my.yorku.ca>
2022-03-29 16:50:46 -0400

    update lab 3

--- sample_site/app.py
+++ sample_site/app.py
@@ -32,8 +32,40 @@ def index():
     f.write(str(winners))
     f.close()
 
+    # Load current 
+    f = open("ifWin.txt", "r")
+    ifWin = str(f.read())
+    f.close()
+
+    # Increment the 
+    if count % 3 == 0:
+        ifWin = "Congratulations you have won!"
+    else:
+        ifWin = "No win"
+
+    # Overwrite the 
+    f = open("ifWin.txt", "w")
+    f.write(str(ifWin))
+    f.close()
+
+        # Load current 
+    f = open("winGif.txt", "r")
+    winGif = str(f.read())
+    f.close()
+
+    # Increment the 
+    if count % 3 == 0:
+        winGif = "https://c.tenor.com/YWvl8RmENwoAAAAC/baby-kotarou.gif"
+    else:
+        winGif = ""
+
+    # Overwrite the 
+    f = open("winGif.txt", "w")
+    f.write(str(winGif))
+    f.close()
+    
     # Render HTML with count and winner variables
-    return render_template("index.html", count=count, winners=winners)
+    return render_template("index.html", count=count, winners=winners, ifWin=ifWin, winGif=winGif)
 
 
 
